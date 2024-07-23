2d06e34b2eaf86f7c5dc66f3e8ff729cbc0db906
ToudaKaito <Kaito.t1109@gmail.com>
2024-07-23 23:49:20 +0900

    ProductRepositoryインターフェースと実装を追加
    - 商品管理のためのIProductRepositoryインターフェースを作成：
      - GetProductById(int productId)
      - SearchProducts(string keyword, string category)
      - AddProduct(Product product)
      - UpdateProduct(Product product)
    - 以下のメソッドを含むProductRepositoryクラスを実装：
      - GetProductById: 商品IDで商品を取得。商品が見つからない場合はNullReferenceExceptionをスロー。
      - SearchProducts: キーワードとカテゴリーで商品を検索。条件に一致する商品のリストを返す。
      - AddProduct: 新しい商品を追加。商品がnullの場合はArgumentNullExceptionをスロー。
      - UpdateProduct: 既存の商品情報を更新。商品がnullの場合はArgumentNullExceptionをスロー。
    - メソッド内にnullチェックを追加して堅牢性を確保。
    - Program.csにIProductRepositoryとProductRepositoryの依存性注入を設定。

---
+++ RaremintShop/Repositories/IProductRepository.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using RaremintShop.Models;
+
+namespace RaremintShop.Repositories
+{
+    /// <summary>
+    /// 商品情報を管理するためのリポジトリインターフェース
+    /// </summary>
+    public interface IProductRepository
+    {
+        /// <summary>
+        /// 商品IDに基づいて商品を取得します。
+        /// </summary>
+        /// <param name="productId">取得する商品のID</param>
+        /// <returns>商品エンティティ</returns>
+        Product GetProductById(int productId);
+
+        /// <summary>
+        /// キーワードとカテゴリーに基づいて商品を検索します。
+        /// </summary>
+        /// <param name="keyword">検索するキーワード</param>
+        /// <param name="category">検索するカテゴリー</param>
+        /// <returns>商品のリスト</returns>
+        IEnumerable<Product> SearchProducts(string keyword, string category);
+
+        /// <summary>
+        /// 新しい商品を追加します。
+        /// </summary>
+        /// <param name="product">追加する商品エンティティ</param>
+        void AddProduct(Product product);
+
+        /// <summary>
+        /// 既存の商品情報を更新します。
+        /// </summary>
+        /// <param name="product">更新する商品エンティティ</param>
+        void UpdateProduct(Product product);
+    }
+}
