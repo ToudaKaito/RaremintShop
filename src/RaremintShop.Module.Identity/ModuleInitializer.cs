d22232d2c96095d81e7e68945c0bc9998769d124
ToudaKaito <156283431+ToudaKaito@users.noreply.github.com>
2025-01-01 17:09:02 +0900

    2025/01/01
    IdentityモジュールのModuleInitializerのコメントの修正など。

--- src/RaremintShop.Module.Identity/ModuleInitializer.cs
+++ src/RaremintShop.Module.Identity/ModuleInitializer.cs
@@ -9,38 +9,25 @@ using RaremintShop.Module.Identity.Data;
 
 namespace RaremintShop.Module.Identity
 {
-    /// <summary>
-    /// Identityモジュールの初期化処理を行うクラス。
-    /// モジュール固有のサービスやデータベースの設定を行う。
-    /// </summary>
     public class ModuleInitializer : IModuleInitializer
     {
-        /// <summary>
-        /// Identityモジュールに必要なサービスを登録するメソッド。
-        /// データベースコンテキストやサービスの依存性注入を設定する。
-        /// </summary>
-        /// <param name="services">DIコンテナにサービスを登録するための IServiceCollection</param>
-        /// <param name="configuration">アプリケーションの設定情報</param>
         public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
         {
-            // Catalogモジュール用のデータベースコンテキストを登録し、MySQLを使用
+            // IdentityDbContextの登録
             services.AddDbContext<IdentityDbContext>(options =>
                 options.UseMySql(
-                    configuration.GetConnectionString("DefaultConnection"), // WebHostの接続文字列を利用
+                    configuration.GetConnectionString("DefaultConnection"),
                     new MySqlServerVersion(new Version(8, 0, 36))));
 
-            // ASP.NET Core Identity の設定を行う
+
+            // Identityフレームワークの構成およびサービスの登録
             services.AddIdentity<IdentityUser, IdentityRole>()
                 .AddEntityFrameworkStores<IdentityDbContext>()
                 .AddDefaultTokenProviders();
+
+
         }
 
-        /// <summary>
-        /// Identityモジュールのルーティングやミドルウェアの設定を行うメソッド。
-        /// 必要に応じて、アプリケーションに特定のミドルウェアやルーティングを追加する。
-        /// </summary>
-        /// <param name="app">アプリケーションの IApplicationBuilder</param>
-        /// <param name="env">アプリケーションのホスティング環境</param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             // 必要に応じてルーティングやミドルウェアの設定を行う
